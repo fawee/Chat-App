b0029b778f236156461922821b65bf498f82e7ec
Fabian Fuchs <fabian1.fuchs@stud.uni-regensburg.de>
2017-03-19 14:00:05 +0100

    Show online Users

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+/* MODULES */
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+/* VARIABLES */
+var users = [];
+
 
 app.use(express.static('public'));
 
@@ -10,17 +15,26 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
+
+    socket.emit("online", users);
+
     socket.on("new_user", function(username) {
         socket.username = username;
+        users.push(username);
         socket.broadcast.emit("connected", socket.username);
     });
 
     socket.on('disconnect', function() {
+
+        var i = users.indexOf("" + socket.username);
+        if (i != -1) {
+            users.splice(i, 1);
+        }
         socket.broadcast.emit("disconnected", socket.username);
     });
 
     socket.on('msg', function(msg) {
-        io.emit('msg', {
+        socket.broadcast.emit('msg', {
             username: socket.username,
             msg: msg
         });
--- public/js/app.js
+++ public/js/app.js
@@ -2,24 +2,27 @@ var App = (function() {
     var that = {},
         socket,
         messages = document.getElementById("messages"),
-        nickname = document.getElementById("nickname");
+        nickname = document.getElementById("nickname"),
+        onlineUsersList = document.getElementById("dropdown-users");
 
     function init() {
         var btn_send = document.getElementById("btn_send"),
-        	btn_nickname = document.getElementById("btn_nickname"),
+            btn_nickname = document.getElementById("btn_nickname"),
             msg = document.getElementById("msg"),
             modal = document.getElementById("modal");
 
         $('#modal').modal('show');
 
         btn_nickname.addEventListener("click", function() {
-        	$('#modal').modal('hide');
-        	socket = io();
-        	initEventListener();
-        	socket.emit("new_user", nickname.value);
+            $('#modal').modal('hide');
+            socket = io();
+            initEventListener();
+            addUserToOnlineUsers(nickname.value);
+            socket.emit("new_user", nickname.value);
         });
 
         btn_send.addEventListener("click", function() {
+            createMsgElement({ msg: msg.value, username: socket.id });
             socket.emit("msg", msg.value);
         });
         console.log("init Client");
@@ -27,30 +30,77 @@ var App = (function() {
 
     function initEventListener() {
 
-    	socket.on("connect", function() {
-    		socket.id = nickname.value;
-    	});
-
-        socket.on("msg", function(data) {
-            var msgElement = document.createElement("li");
-            msgElement.className = "well";
-            msgElement.innerHTML = data.username + ": " + data.msg;
-            messages.appendChild(msgElement);
+        socket.on("connect", function() {
+            socket.id = nickname.value;
         });
 
-        socket.on("connected", function(user) {
-            var msgElement = document.createElement("li");
-            msgElement.className = "well connected";
-            msgElement.innerHTML = user + " joined the chat room";
-            messages.appendChild(msgElement);
+        socket.on("online", function(users) {
+            if (users.length != 0) {
+                addUserToOnlineUsers(users);
+            }
+        });
+
+        socket.on("msg", function(data) {
+            createMsgElement({ msg: data.msg, username: data.username });
+        });
+
+        socket.on("connected", function(username) {
+            addUserToOnlineUsers(username);
+            createMsgElement({ username: username, connected: true });
         });
 
         socket.on("disconnected", function(user) {
-            var msgElement = document.createElement("li");
-            msgElement.className = "well disconnected";
-            msgElement.innerHTML = user + " has left the chat room";
-            messages.appendChild(msgElement);
+            createMsgElement({ username: user, connected: false })
+            removeUserFromOnlineUsers(user);
         });
+    }
+
+    function createMsgElement(options) {
+        var msgElement = document.createElement("li");
+
+        if (options.msg) {
+            msgElement.className = "well";
+            msgElement.innerHTML = options.username + ": " + options.msg;
+        } else if (options.connected) {
+            msgElement.className = "well connected";
+            msgElement.innerHTML = options.username + " has joined the chat room";
+        } else {
+            msgElement.className = "well disconnected";
+            msgElement.innerHTML = options.username + " has left the chat room";
+        }
+
+        messages.appendChild(msgElement);
+    }
+
+    function addUserToOnlineUsers(user) {
+
+        if (Array.isArray(user)) {
+            user.forEach((user) => {
+                var userElement = document.createElement("li");
+                var userLink = document.createElement("a");
+
+                userElement.appendChild(userLink);
+                userElement.id = user;
+                userLink.href = "#";
+                userLink.innerHTML = user;
+                onlineUsersList.appendChild(userElement);
+            });
+        } else {
+            var userElement = document.createElement("li"),
+                userLink = document.createElement("a");
+
+            userElement.appendChild(userLink);
+            userElement.id = user;
+            userLink.href = "#";
+            userLink.innerHTML = user;
+            onlineUsersList.appendChild(userElement);
+        }
+    }
+
+    function removeUserFromOnlineUsers(user) {
+    	console.log(user);
+    	var userElement = document.getElementById("" + user);
+    	userElement.parentNode.removeChild(userElement);
     }
 
     that.init = init;
